88b2eb0d0a28c54ed86d72a1519433cd87b3c069
grreuze <grreuze@gmail.com>
2017-01-20 19:30:57 +0100

    Unity folder

---
+++ Assets/DraggableObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DraggableObject : MonoBehaviour {
+    
+    /// <summary>
+    /// Returns whether or not the object is currently held by the player.
+    /// </summary>
+            
+    public bool isHeld;
+    
+	void Update () {
+		if (isHeld) MoveToMousePosition();
+	}
+
+    void MoveToMousePosition() {
+        transform.position = Input.mousePosition;
+    }
+
+    void OnMouseOver() {
+        isHeld = Input.GetMouseButton(0);
+    }
+
+    void OnMouseExit() {
+        isHeld = !Input.GetMouseButton(0);
+    }
+
+}
